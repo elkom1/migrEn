798d94b2b89da39495f685189495ab674b4457ae
Unknown <besio1@bfh.ch>
2019-01-02 15:27:49 +0100

    persist of first 2 diagnosis to midata and date

--- src/pages/menu_Diagnosen/menu_Diagnosen.ts
+++ src/pages/menu_Diagnosen/menu_Diagnosen.ts
@@ -17,6 +17,7 @@ import {
 })
 export class Diagnosen {
 
+  date = Date; 
   diagnosen: string; 
   otherDiagnose: string; 
 
@@ -38,5 +39,81 @@ export class Diagnosen {
       buttons: ['OK']
     });
     alert.present();
+
+    //========================= START JSON FOR THE OBSERVATION "Diagnosis"================================
+    let codingStuff2 = {
+      coding: [{
+        system: 'http://snomed.info/sct',
+        code: '418138009',
+        display: 'Diagnosis' //muss noch registriert werden
+      }]
+    }
+
+    let category2 = {
+      coding: [{
+        system: 'http://hl7.org/fhir/observation-category',
+        code: 'survey',
+        display: 'Survey'
+      }],
+    }
+
+    let entry2 = new Observation({
+      _dateTime: new Date().toISOString()
+    }, codingStuff2, category2);
+
+    if (this.diagnosen != null) {
+
+      if (this.diagnosen.match("Migräne mit Aura")) {
+        entry2.addComponent({
+          code: {
+            coding: [{
+              system: "http://snomed.info/sct",
+              code: "4473006",
+              display: "Migraine with aura"
+            }]
+          },
+        })
+      }
+
+      if (this.diagnosen.match("Migräne ohne Aura")) {
+        entry2.addComponent({
+          code: {
+            coding: [{
+              system: "http://snomed.info/sct",
+              code: "56097005",
+              display: "Migraine without aura"
+            }]
+          },
+        })
+      }
+
+      if(this.diagnosen.match("Andere")) {
+        entry2.addComponent({
+          code: {
+            coding: [{
+              display: "Other diagnosis"
+            }]
+          },
+          valueString: this.otherDiagnose
+        })
+      }
+
+      if (this.date != null) {
+        entry2.addComponent({
+          code: {
+            coding: [{
+              display: "Date of diagnosis"
+            }]
+          },
+          valueDateTime: "" + this.date
+        })
+      }
+
+      let bundle2 = new Bundle("transaction");
+      bundle2.addEntry("POST", entry2.resourceType, entry2);
+      this.midataService.save(bundle2);
+    }
+    //========================= END JSON FOR THE OBSERVATION "Diagnosis"================================
+
   }
 }
