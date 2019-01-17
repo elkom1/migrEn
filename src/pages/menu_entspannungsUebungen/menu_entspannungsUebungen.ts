f855b82f2f88cd21def91d8a18cb874d98f97459
Unknown <musabgamal.elkour@students.bfh.ch>
2019-01-17 15:33:49 +0100

    Relaxation exercises added

--- src/pages/menu_entspannungsUebungen/menu_entspannungsUebungen.ts
+++ src/pages/menu_entspannungsUebungen/menu_entspannungsUebungen.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {
+  Component
+} from '@angular/core';
+import {
+  NavController
+} from 'ionic-angular';
 
 @Component({
   selector: 'page-entspannungsUebungen',
@@ -7,7 +11,34 @@ import { NavController } from 'ionic-angular';
 })
 export class EntspannungsUebungen {
 
-  constructor(public navCtrl: NavController) {
+  selectedCard: Boolean = false;
+  selectedCard2: Boolean = false;
+  selectedCard3: Boolean = false;
+
+  constructor(public navCtrl: NavController) {}
+
+  showText() {
+    if (this.selectedCard == false) {
+      this.selectedCard = true;
+    } else {
+      this.selectedCard = false;
+    }
+  }
+
+  showText2() {
+    if (this.selectedCard2 == false) {
+      this.selectedCard2 = true;
+    } else {
+      this.selectedCard2 = false;
+    }
+  }
+
+  showText3() {
+    if (this.selectedCard3 == false) {
+      this.selectedCard3 = true;
+    } else {
+      this.selectedCard3 = false;
+    }
   }
 
 }
